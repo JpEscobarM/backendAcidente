24691eac40515f84d47f5ef62307477bf1159651
JpEscobarM <jp.escobarmartins@gmail.com>
2025-05-23 11:47:19 -0300

    update API
    add CORS

--- api/accident.js
+++ api/accident.js
@@ -1,6 +1,15 @@
 const db = require("../lib/db");
 
-module.exports = (req, res) => {
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://acidente-planetaagua.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method === "GET") {
     db.get("SELECT date FROM accidents ORDER BY id DESC LIMIT 1", [], (err, row) => {
       if (err) return res.status(500).json({ error: err.message });
@@ -35,4 +44,4 @@ module.exports = (req, res) => {
   } else {
     res.status(405).end(); 
   }
-};
+}
