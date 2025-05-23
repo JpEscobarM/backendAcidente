50591250806c4b711289c26adb797ce8ae486227
JpEscobarM <jp.escobarmartins@gmail.com>
2025-05-23 11:55:03 -0300

    update

--- api/record.js
+++ api/record.js
@@ -1,7 +1,7 @@
 const db = require("../lib/db");
 
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://acidente-planetaagua.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
